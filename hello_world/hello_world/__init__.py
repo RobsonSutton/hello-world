918969b212e7bd3f15b7a51e30fcf7fb903f3f79
Robson Sutton <robson.sutton@nationwide.co.uk>
2022-02-16 20:34:12 +0000

    uplift app from using packages to blueprints

--- hello_world/hello_world/__init__.py
+++ hello_world/hello_world/__init__.py
@@ -1,10 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from hello_world.config import Config
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "5a159e3ead7552a79f5c256ab9fc7d13" # Set as env vars in the future
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
 
-from hello_world import routes
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = "users.login"
+login_manager.login_message_category = "info"
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    #mail.init_app(app)
+
+    from hello_world.users.routes import users
+    from hello_world.posts.routes import posts
+    from hello_world.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
